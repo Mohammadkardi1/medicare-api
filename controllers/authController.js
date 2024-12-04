9250d52cd34fe9c72c9f1fa2a944940fe9fb863a
MohammadKardi1 <mohammadkardi1@gmail.com>
2024-12-04 15:20:07 +0100

    update authentication controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,16 +21,12 @@ export const register = async (req, res) => {
     if (!name || !email || !password || !role) {
         return res.status(400).json({message:"Required fields are missing"})
     }
-
-
     if (!nameValidator(name.trim())) {
         return res.status(400).json({message: 'Name must be alphanumeric at least 4 characters long'})
     }
-
     if (!emailValidator(email)) {
         return res.status(400).json({message: "Email is not valid"})
     }
-
     if (!passwordValidator(password)) {
         return res.status(400).json({message: 'Password is not strong enough'})
     }
@@ -91,14 +87,14 @@ export const login = async (req, res) => {
 
         // Check if the user exsits in Database
         if (!user) {
-            return res.status(404).json({success: false, message: "User not found! Please Sign up."})
+            return res.status(404).json({success: false, message: "User not found! Please Sign up"})
         }
 
         // Compare password
         const isPasswordMatch = await bcrypt.compare(req.body.password, user.password)
 
         if (!isPasswordMatch) {
-            return res.status(400).json({status: false, message: "Invalid credentials."})
+            return res.status(400).json({status: false, message: "The password you entered is incorrect"})
         }
 
 
@@ -111,8 +107,8 @@ export const login = async (req, res) => {
 
         const {password, role, appointments, ...rest} = user._doc
 
-        return res.status(200).json({status: true, message:"You have been logged in Successfully.", token, data:{...rest}, role})
+        return res.status(200).json({status: true, message:"You have been logged in Successfully", token, data:{...rest}, role})
     } catch (error) {
-        return res.status(500).json({status: false, message:"Internal server error. Please try again later."})
+        return res.status(500).json({status: false, message:"Internal server error. Please try again later"})
     }
 }
