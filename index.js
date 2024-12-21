14d36de16f2cc46e1ded4459441c4bda7b41f152
MohammadKardi1 <mohammadkardi1@gmail.com>
2024-12-21 18:01:18 +0100

    update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const CONNECTION_URL = process.env.MONGO_URL
 // Middleware
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173', 'https://medicare-client-mocha.vercel.app/'] 
+    origin: ['http://localhost:5173', 'https://medicare-client-mocha.vercel.app'] 
 }))
 app.use(cookieParser())
 app.use(express.json())
